19ac80233858c1963211670755d7af465b648e85
kimimaro <kimimaro1952@hotmail.com>
2017-07-23 21:16:32 +0700

    Connect New

--- include/config.inc.php
+++ include/config.inc.php
@@ -4,12 +4,16 @@
 	$_config['database']['username'] = "root";
 	$_config['database']['password'] = "root1234";
 	$_config['database']['database'] = "api3";
-	
+	# configuration for database		
+	// define('DB_SERVER', 'localhost');
+	// define('DB_USERNAME', 'root');
+	// define('DB_PASSWORD', 'root1234');
+	// define('DB_DATABASE', 'api3');
 	# connect the database server
 	$link = new mysqldb();
 	$link->connect($_config['database']);
 	$link->selectdb($_config['database']['database']);
-	//$link->query("SET NAMES 'utf8'");
-	//echo 'desfefsf';
+	$link->query("SET NAMES 'utf8'");
+
 	@session_start();
 ?>
